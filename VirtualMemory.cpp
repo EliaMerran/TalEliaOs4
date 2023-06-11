e8c9c638cd8a5496195a5068cfd61220d8753550
eliamerran <elia.merran@mail.huji.ac.il>
2023-06-11 15:31:35 +0300

    read naive implementation

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -3,8 +3,14 @@
 //
 #pragma once
 #include "VirtualMemory.h"
+#include "PhysicalMemory.h"
 
 // TODO : Remove io stream !
+
+void getTablesIndex(uint64_t virtualAddress, uint64_t** partitions, int num_partitions)
+{
+
+}
 
 /*
  * Initialize the virtual memory.
@@ -23,8 +29,31 @@ void VMinitialize()
  */
 int VMread(uint64_t virtualAddress, word_t* value)
 {
-
+  int num_partitions = TABLES_DEPTH + 1;
+  uint64_t partitions[num_partitions];
+  // split the virtual address into partitions of OFFSET_WIDTH bits
+  uint64_t mask = (1 << OFFSET_WIDTH) - 1;
+  for (int i = 0; i < num_partitions; i++)
+    {
+      partitions[i] = virtualAddress & mask;
+      virtualAddress >>= OFFSET_WIDTH;
+    }
+    // get the physical address of the page table
+    uint64_t frame_base = 0;
+    for (int i = 0; i < num_partitions - 1; ++i)
+    {
+      // cast frame_base to word_t* to use PMread
+      PMread(frame_base + partitions[i], (word_t*) &frame_base);
+    }
+    // get the physical address of the page
+    uint64_t physical_address = frame_base + partitions[num_partitions - 1];
+    // read the value from the physical address
+    PMread(physical_address, value);
+    return 1;
 }
+
+
+
 
 /* Writes a word to the given virtual address.
  *
--- cmake-build-debug/CMakeFiles/TalEliaOs4.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/TalEliaOs4.dir/cmake_clean.cmake
@@ -1,10 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o"
-  "CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o.d"
   "CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o"
-  "CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o.d"
   "CMakeFiles/TalEliaOs4.dir/VirtualMemory.cpp.o"
-  "CMakeFiles/TalEliaOs4.dir/VirtualMemory.cpp.o.d"
   "TalEliaOs4"
   "TalEliaOs4.pdb"
 )
