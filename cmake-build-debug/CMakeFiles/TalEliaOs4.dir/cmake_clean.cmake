6d69e94ee008a749e0cf17f90b53c04587c2b98c
eliamerran <elia.merran@mail.huji.ac.il>
2023-06-11 12:16:37 +0300

    resources

---
+++ cmake-build-debug/CMakeFiles/TalEliaOs4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o"
+  "CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o"
+  "TalEliaOs4"
+  "TalEliaOs4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TalEliaOs4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
