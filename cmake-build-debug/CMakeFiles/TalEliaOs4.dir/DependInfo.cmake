5f842347b7534aeeacc4b2f3db88f9303eeb39de
suchetzky <tal.suchetzky@mail.huji.ac.il>
2023-06-11 14:42:17 +0300

    first

--- cmake-build-debug/CMakeFiles/TalEliaOs4.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/TalEliaOs4.dir/DependInfo.cmake
@@ -1,17 +1,16 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/c/UniCode/os/TalEliaOs4/PhysicalMemory.cpp" "/mnt/c/UniCode/os/TalEliaOs4/cmake-build-debug/CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o"
-  "/mnt/c/UniCode/os/TalEliaOs4/SimpleTest.cpp" "/mnt/c/UniCode/os/TalEliaOs4/cmake-build-debug/CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../."
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/cs/usr/suchetzky/os/TalEliaOs4/PhysicalMemory.cpp" "CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o" "gcc" "CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o.d"
+  "/cs/usr/suchetzky/os/TalEliaOs4/SimpleTest.cpp" "CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o" "gcc" "CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o.d"
+  "/cs/usr/suchetzky/os/TalEliaOs4/VirtualMemory.cpp" "CMakeFiles/TalEliaOs4.dir/VirtualMemory.cpp.o" "gcc" "CMakeFiles/TalEliaOs4.dir/VirtualMemory.cpp.o.d"
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/TalEliaOs4.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/TalEliaOs4.dir/cmake_clean.cmake
@@ -1,6 +1,10 @@
 file(REMOVE_RECURSE
   "CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o"
+  "CMakeFiles/TalEliaOs4.dir/PhysicalMemory.cpp.o.d"
   "CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o"
+  "CMakeFiles/TalEliaOs4.dir/SimpleTest.cpp.o.d"
+  "CMakeFiles/TalEliaOs4.dir/VirtualMemory.cpp.o"
+  "CMakeFiles/TalEliaOs4.dir/VirtualMemory.cpp.o.d"
   "TalEliaOs4"
   "TalEliaOs4.pdb"
 )
