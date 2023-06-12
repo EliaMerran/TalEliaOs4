a8b852588c63af40e7eb4f030974031958a5f977
eliamerran <elia.merran@mail.huji.ac.il>
2023-06-12 14:55:04 +0300

    write and read,2 steps to find frame to evict

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -7,9 +7,105 @@
 
 // TODO : Remove io stream !
 
-void getTablesIndex(uint64_t virtualAddress, uint64_t** partitions, int num_partitions)
+void initFrame (word_t FrameIndex);
+word_t findFrameToEvict ();
+void findMaxDepth (int i, int *p_int);
+word_t handlePageFault(uint64_t virtualAddress)
 {
+  word_t FrameIndex = findFrameToEvict();
+  // evict page from frame
+  PMrestore (FrameIndex, virtualAddress >> OFFSET_WIDTH);
+}
+word_t findFrameToEvict ()
+{
+  // step 1 : check if there is an empty frame
+  word_t value;
+  bool isFrameEmpty;
+  for (int i = 1; i < NUM_FRAMES; i++)
+    {
+      isFrameEmpty = true;
+      for (int j = 0; j < PAGE_SIZE; ++j)
+        {
+          PMread (i * PAGE_SIZE + j, &value);
+          if (value != 0)
+            {
+              isFrameEmpty = false;
+              break;
+            }
+        }
+      if (isFrameEmpty)
+        {
+          return i;
+        }
+    }
 
+  // step 2 : find the max depth of the tree
+  int maxDepth = 0;
+  findMaxDepth (0, &maxDepth);
+  if (maxDepth + 1 < NUM_FRAMES)
+    {
+      return maxDepth + 1;
+    }
+  // step 3 :
+}
+void findMaxDepth (int frameIndex, int *depth)
+{
+  if (frameIndex == TABLES_DEPTH)
+    return;
+  word_t value;
+  for (int j = 0; j < PAGE_SIZE; ++j)
+    {
+      PMread (frameIndex * PAGE_SIZE + j, &value);
+      if (value != 0)
+        {
+          if (value > *depth)
+            {
+              *depth = value;
+            }
+          findMaxDepth (value, depth);
+        }
+    }
+}
+
+void getPhysicalAddress(uint64_t virtualAddress, uint64_t *physicalAddress)
+{
+  int num_partitions = TABLES_DEPTH + 1;
+  uint64_t partitions[num_partitions];
+  // split the virtual address into partitions of OFFSET_WIDTH bits
+  uint64_t mask = (1 << OFFSET_WIDTH) - 1; // ..00001111
+  for (int i = 0; i < num_partitions; i++)
+    {
+      partitions[i] = virtualAddress & mask;
+      virtualAddress >>= OFFSET_WIDTH;
+    }
+  // get the physical address of the page table
+  uint64_t frame_base = 0;
+  for (int i = num_partitions - 1; i > 0; i--)
+    {
+      // cast frame_base to word_t* to use PMread
+      uint64_t address = frame_base + partitions[i];
+      PMread(address, (word_t*) &frame_base);
+      if (frame_base == 0)
+        {
+          // page not found
+          //Handle bring page from disk
+          word_t newFrameIndex = handlePageFault (virtualAddress);
+          PMwrite (address, newFrameIndex);
+          frame_base = newFrameIndex;
+          initFrame (newFrameIndex);
+
+        }
+      frame_base *= PAGE_SIZE;
+    }
+  // get the physical address of the page
+  *physicalAddress = frame_base + partitions[0];
+}
+void initFrame (word_t FrameIndex)
+{
+  for (int i = 0; i < PAGE_SIZE; i++)
+    {
+      PMwrite (FrameIndex * PAGE_SIZE + i, 0);
+    }
 }
 
 /*
@@ -17,7 +113,7 @@ void getTablesIndex(uint64_t virtualAddress, uint64_t** partitions, int num_part
  */
 void VMinitialize()
 {
-
+  initFrame (0);
 }
 
 /* Reads a word from the given virtual address
@@ -29,27 +125,11 @@ void VMinitialize()
  */
 int VMread(uint64_t virtualAddress, word_t* value)
 {
-  int num_partitions = TABLES_DEPTH + 1;
-  uint64_t partitions[num_partitions];
-  // split the virtual address into partitions of OFFSET_WIDTH bits
-  uint64_t mask = (1 << OFFSET_WIDTH) - 1;
-  for (int i = 0; i < num_partitions; i++)
-    {
-      partitions[i] = virtualAddress & mask;
-      virtualAddress >>= OFFSET_WIDTH;
-    }
-    // get the physical address of the page table
-    uint64_t frame_base = 0;
-    for (int i = 0; i < num_partitions - 1; ++i)
-    {
-      // cast frame_base to word_t* to use PMread
-      PMread(frame_base + partitions[i], (word_t*) &frame_base);
-    }
-    // get the physical address of the page
-    uint64_t physical_address = frame_base + partitions[num_partitions - 1];
-    // read the value from the physical address
-    PMread(physical_address, value);
-    return 1;
+  uint64_t physicalAddress;
+  getPhysicalAddress (virtualAddress, &physicalAddress);
+  // read the value from the physical address
+  PMread(physicalAddress, value);
+  return 1;
 }
 
 
@@ -63,5 +143,9 @@ int VMread(uint64_t virtualAddress, word_t* value)
  */
 int VMwrite(uint64_t virtualAddress, word_t value)
 {
-
+  uint64_t physicalAddress;
+  getPhysicalAddress (virtualAddress, &physicalAddress);
+  // read the value from the physical address
+  PMwrite(physicalAddress, value);
+  return 1;
 }
